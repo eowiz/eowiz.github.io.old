907c62a982ccc0d1b132c622ff948f803a106ce2
eowiz <eowiz@proton.me>
2023-01-16 02:41:21 +0900

    feat: add remark-emoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import emoji from "remark-emoji";
+
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [emoji],
+    extendDefaultPlugins: true,
+  },
   integrations: [tailwind()],
 });
